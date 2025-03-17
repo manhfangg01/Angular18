3fd7e6f8a70059d1e9b47d2ef00890069c3b4d0c
Manh Phan <imseirousmanh@gmail.com>
2025-03-17 09:58:43 +0700

    #4: create header-layout-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from './shared/header-layout/header-layout-component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/shared/header-layout/header-layout-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'header-layout',
+  standalone: true, // Đánh dấu 1 component hoạt động độc lập
+  templateUrl: './header-layout-component.html',
+  styleUrl: './header-layout-component.css',
+})
+export class HeaderLayoutComponent {}
