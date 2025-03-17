3849e52140a33f8119de90a730644f7451b99c2f
Manh Phan <imseirousmanh@gmail.com>
2025-03-17 21:02:37 +0700

    #6 Dynamic text, HTML properties, Css Attributes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,17 @@ import { HeaderLayoutComponent } from './shared/header-layout/header-layout-comp
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+// Những biến được khai báo ở đây có thể dụng ở view tương ứng
 export class AppComponent {
+  // text
   title = 'angular-basic-project';
+  obj = { ten: 'Manh', tuoi: 20 };
+
+  // Properties
+  isDisable = true;
+
+  // Attributes
+  contentImage = 'Hello World';
 }
 
 // File chứa các import (Component xử lý các logic, nhúng file)
