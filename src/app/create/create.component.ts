a3c5173325adf4ca308d965b47d294e4cc50f391
Manh Phan <imseirousmanh@gmail.com>
2025-03-21 23:30:12 +0700

    #29 Form Control

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -4,31 +4,21 @@ import { BlogService } from '../../service/BlogService';
 import { ProductItems } from '../types/productItem';
 import { currencyPipe } from '../shared/pipes/CurrencyPipe.pipe';
 import { NgIf } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css',
 })
-export class CreateComponent implements OnInit {
-  id = '';
-  productItem: ProductItems = {
-    id: 0,
-    image: '',
-    name: '',
-    price: 0,
-  };
-
+export class CreateComponent {
+  name = new FormControl('');
+  price = new FormControl('');
   constructor(private blogService: BlogService) {}
 
-  ngOnInit(): void {
-    this.blogService.detailBlog(+this.id).subscribe(({ data }: any) => {
-      this.productItem.id = data.id;
-      this.productItem.name = data.title;
-      this.productItem.price = data.body;
-      this.productItem.image = 'assets/images/shoe-4.jpg';
-    });
+  handleAddCart() {
+    console.log(this.name.value);
   }
 }
